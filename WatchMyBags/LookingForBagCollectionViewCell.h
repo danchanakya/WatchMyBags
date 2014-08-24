05683ad356ce7df4754c29cb6915eb55bd799c1b
Dantha Manikka-Baduge <danthac@gmail.com>
2014-08-24 16:19:44 -0700

    first commit to github

---
+++ WatchMyBags/LookingForBagCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  LookingForBagCollectionViewCell.h
+//  WatchMyBags
+//
+//  Created by Dantha Manikka-Baduge on 8/24/14.
+//  Copyright (c) 2014 Dantha Manikka-Baduge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LookingForBagCollectionViewCell : UICollectionViewCell
+
+@end
